ffdbe65bc7b87e61657eee9a36610f46ea6a93e1
brownjs03 <brownjs03@gmail.com>
2019-11-04 21:56:18 -0500

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -20,24 +20,24 @@ attr_accessor :board
         @index = input.to_i - 1
     end
 
-    def move(index, token = "x")
+    def move(index, token)
         @board[index] = token
     end 
 
     def position_taken?(index)
-        @board[index] != " "
+        @board[index] == "X" || @board[index] == "O"
     end
 
-    def valid_move?(position)
-        @board[position] == " "
+    def valid_move?(index)
+        index.between?(0,8) && position_taken?(index) == false
     end 
     
     def turn_count
         counter = 0
         @board.each do |space|
-            if space != " "
-                counter += 1
-            end
+            if space == "X" || space == "O"
+               counter += 1
+           end
         end
         counter
     end 
@@ -47,13 +47,74 @@ attr_accessor :board
     end
 
     def turn
-        puts "Enter 1-9"
-        move = gets 
-        index = input_to_index(move)
-        while valid_move?(index)
+        puts "Please enter 1-9"
+        input = gets.strip
+        index = input_to_index(input)
+        if valid_move?(index)
             move(index, current_player)
+            display_board
+        else
+            turn
         end
-        display_board
     end
-end
+
+    def won?
+        WIN_COMBINATIONS.each {|combo|
+        spot1 = combo[0]
+        spot2 = combo[1]
+        spot3 = combo[2]
+    
+        position1 = board[spot1]
+        position2 = board[spot2]
+        position3 = board[spot3]
+    
+        if position1 == "X" && position2 == "X" && position3 == "X"
+            @winner = "X"
+          return combo
+        elsif position1 == "O" && position2 == "O" && position3 == "O"
+            @winner = "O"
+          return combo
+        end
+      }
+      return false
+    end
+
+    def full?
+       board.all? {|position| position == "X" || position == "O"}
+    end
+
+    def draw?
+        if !won? && full?
+            true
+        else
+            false
+        end
+    end
+
+    def over?
+        if won? || draw?
+            true
+        else
+            false
+        end
+    end
+
+    def winner
+        if won? == false
+            nil
+        else
+            @winner
+        end
+    end
+
+    def play
+        while over? == false
+            turn
+        end
+        if won? 
+            puts "Congratulations #{@winner}!"
+        elsif draw?
+            puts "Cat's Game!"
+        end
+    end 
 end
